795172aa6365bd9816ed16e6b739e2d66668d70e
Marvin Knabe <marvin.boeddeker@googlemail.com>
2019-12-12 13:28:15 +0100

    Added Tests for ChartItem

--- pod_api/bin/app/models/chart_item.dart
+++ pod_api/bin/app/models/chart_item.dart
@@ -1,4 +1,29 @@
+import 'package:xml/xml.dart' as xml;
 import 'package:meta/meta.dart';
+
+class ChartsResponse {
+  List<ChartItem> entries;
+
+  ChartsResponse({@required String xmlString}) {
+    this.entries = _mapEntries(xmlString);
+  }
+
+  List<ChartItem> _mapEntries(String xmlString) {
+    try {
+      final xml.XmlDocument document = xml.parse(xmlString);
+
+      var chartItems = document
+          .findAllElements("entry")
+          .map<ChartItem>((element) => ChartItem.fromXml(element))
+          .toList();
+
+      return chartItems;
+    } catch (err) {
+      print(err);
+      return List<ChartItem>();
+    }
+  }
+}
 
 class ChartItem {
   final String id;
@@ -18,4 +43,24 @@ class ChartItem {
     @required this.releaseDate,
     @required this.category,
   });
+
+  static ChartItem fromXml(xml.XmlElement xmlNode) {
+    var id = xmlNode.findAllElements('id').single.getAttribute('im:id');
+    var title = xmlNode.findAllElements('title').single.text;
+    var summary = xmlNode.findAllElements('summary').single.text;
+    var artist = xmlNode.findAllElements('im:artist').single.text;
+    var thumbnail = xmlNode.findAllElements('im:image').last.text ?? "";
+    var releaseDate = xmlNode.findAllElements('im:releaseDate').single.text;
+    var category =
+        xmlNode.findAllElements('category').single.getAttribute('label');
+
+    return ChartItem(
+        id: id,
+        title: title,
+        summary: summary,
+        artist: artist,
+        thumbnail: thumbnail,
+        releaseDate: releaseDate,
+        category: category);
+  }
 }
---
+++ pod_api/test/model_tests/chartitem_model_test.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+import 'package:xml/xml.dart' as xml;
+import '../../bin/app/models/chart_item.dart';
+
+void main() {
+  xml.XmlDocument document;
+
+  setUp(() async {
+    final File file = File('test_resources/charts.xml');
+    document = xml.parse(await file.readAsString());
+  });
+
+  tearDown(() {
+    document = null;
+  });
+
+  test('Test xml parsing to ChartItem', () {
+    expect(document, isNotNull);
+
+    var entries = document.findAllElements("entry");
+    expect(entries.length, isNot(0));
+
+    var chartItems = entries
+        .map<ChartItem>((element) => ChartItem.fromXml(element))
+        .toList();
+
+    var firstItem = chartItems.first;
+
+    expect(firstItem, isNotNull);
+
+    expect(firstItem.title, equals('Unter Pfarrerstöchtern - ZEIT ONLINE'));
+    expect(firstItem.category, equals('Religion und Spiritualität'));
+    expect(firstItem.artist, equals('ZEIT ONLINE'));
+    expect(firstItem.releaseDate, equals('2019-12-05T22:30:00-07:00'));
+    expect(firstItem.id, equals('1489988353'));
+    expect(
+        firstItem.thumbnail,
+        equals(
+            'https://is1-ssl.mzstatic.com/image/thumb/Podcasts113/v4/71/a6/97/71a69737-3a89-453a-dca8-ca088e7a7736/mza_6770475896512430452.png/170x170bb.png'));
+  });
+
+  test('Test xml Parsing to ChartResponse', () async {
+    final File file = File('test_resources/charts.xml');
+    final String xmlString = await file.readAsString();
+
+    final sut = ChartsResponse(xmlString: xmlString);
+    expect(sut.entries.length, isNot(0));
+    expect(sut.entries.first.title,
+        equals('Unter Pfarrerstöchtern - ZEIT ONLINE'));
+  });
+}
