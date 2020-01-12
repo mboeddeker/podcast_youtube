7b09f7fb2c0c81f813ae2ed424653288e048f6b7
Marvin Knabe <marvin.boeddeker@gmail.com>
2020-01-12 11:42:03 +0100

    Changed some parts

--- pod_api/bin/main.dart
+++ pod_api/bin/main.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
+import 'package:angel_framework/http.dart';
+
 import 'app/app.dart';
 import 'package:angel_hot/angel_hot.dart';
 
 main() async {
-  final App app = await App();
+  /* final App app = await App();
 
   var hotReloader = HotReloader(app.createServer, [new Directory('bin/app')]);
-  await hotReloader.startServer('localhost', 3000);
-}
+  await hotReloader.startServer('localhost', 3000); */
 
+  final App app = await App();
+  final angelInstance = await app.createServer();
+
+  var http = AngelHttp(angelInstance);
+  await http.startServer('localhost', 3000);
+}
