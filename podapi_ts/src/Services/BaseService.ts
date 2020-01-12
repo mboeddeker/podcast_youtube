b033fb51fd4700f03cb809d29099a3242936c2bc
Marvin Knabe <marvin.boeddeker@gmail.com>
2020-01-12 20:31:44 +0100

    askjfh

--- podapi_ts/src/Services/BaseService.ts
+++ podapi_ts/src/Services/BaseService.ts
@@ -1,5 +1,11 @@
 import PodcastFeed from "Models/PodcastFeed";
+// tslint:disable-next-line: no-var-requires
+import * as request from 'request';
 
 class BaseServie {
-  public getSomething() {}
+  public getSomething() {
+    request('', (error, response, body) {
+      
+    })
+  }
 }
