909a5e6818835c6c4a28a19afe05579a77f6ca72
Marvin Knabe <marvin.boeddeker@gmail.com>
2020-01-25 02:07:37 +0100

    Intro Slides UI Finish

--- podcast_app/lib/pages/intro_slider/views/intro_page.dart
+++ podcast_app/lib/pages/intro_slider/views/intro_page.dart
@@ -33,104 +33,119 @@ class _IntroPageState extends State<IntroPage> {
 
   Widget _buildView(BuildContext context, IntroPageViewModel viewModel) {
     return ListenableProvider<GenericState<int>>(
-        create: (_) => viewModel.provideIndex(),
-        child: Consumer<GenericState<int>>(
-          builder: (context, currentIndex, _) {
-            return Stack(
-              children: [
-                TransformerPageView(
-                  pageSnapping: viewModel.pageSnappingAllowed(),
-                  controller: viewModel.provideIndexController(),
-                  loop: viewModel.loopAllowed(),
-                  itemCount: viewModel.sliderTitles().length,
-                  onPageChanged: (index) => viewModel.onPageChanged(index),
-                  transformer: PageTransformerBuilder(builder: (Widget child, TransformInfo info) {
-                    return Container(
-                      color: Colors.white,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          ParallaxContainer(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 24, right: 24, top: 50),
-                              child: Text(
-                                viewModel.sliderTitles()[currentIndex.value],
-                                style: AppTextStyles.heading1,
-                              ),
+      create: (_) => viewModel.provideIndex(),
+      child: Consumer<GenericState<int>>(
+        builder: (context, currentIndex, _) {
+          return Stack(
+            children: [
+              TransformerPageView(
+                pageSnapping: viewModel.pageSnappingAllowed(),
+                controller: viewModel.provideIndexController(),
+                loop: viewModel.loopAllowed(),
+                itemCount: viewModel.sliderTitles().length,
+                onPageChanged: (index) => viewModel.onPageChanged(index),
+                transformer: PageTransformerBuilder(builder: (Widget child, TransformInfo info) {
+                  return Container(
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        ParallaxContainer(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 24, right: 24, top: 50),
+                            child: Text(
+                              viewModel.sliderTitles()[currentIndex.value],
+                              style: AppTextStyles.heading1,
                             ),
-                            position: info.position,
-                            opacityFactor: 1,
-                            translationFactor: 100.0,
                           ),
-                          SizedBox(height: 32),
-                          ParallaxContainer(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 24, right: 24),
-                              child: Text(
-                                viewModel.sliderSubTitles()[currentIndex.value],
-                                style: AppTextStyles.heading2,
-                              ),
+                          position: info.position,
+                          opacityFactor: 1,
+                          translationFactor: 100.0,
+                        ),
+                        SizedBox(height: 32),
+                        ParallaxContainer(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 24, right: 24),
+                            child: Text(
+                              viewModel.sliderSubTitles()[currentIndex.value],
+                              style: AppTextStyles.heading2,
                             ),
-                            position: info.position,
-                            opacityFactor: 1,
-                            translationFactor: 600.0,
                           ),
-                          SizedBox(height: 24),
-                          ParallaxContainer(
-                            child: Container(
-                              alignment: Alignment.centerRight,
-                              height: MediaQuery.of(context).size.height / 2.1,
-                              child: Image.asset(
-                                viewModel.sliderImages()[currentIndex.value],
-                                fit: BoxFit.contain,
-                              ),
+                          position: info.position,
+                          opacityFactor: 1,
+                          translationFactor: 600.0,
+                        ),
+                        SizedBox(height: 24),
+                        ParallaxContainer(
+                          child: Container(
+                            alignment: Alignment.centerRight,
+                            height: MediaQuery.of(context).size.height / 2.5,
+                            child: Image.asset(
+                              viewModel.sliderImages()[currentIndex.value],
+                              fit: BoxFit.contain,
                             ),
-                            position: info.position,
-                            opacityFactor: 1,
-                            translationFactor: 900.0,
                           ),
-                          currentIndex.value == 2
-                              ? //TODO: this a single Widget
-                              ParallaxContainer(
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      MaterialButton(
-                                        height: 50,
-                                        minWidth: 150,
-                                        onPressed: () {},
-                                        child: Text('OKAY'),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius: new BorderRadius.circular(25.0),
-                                        ),
-                                        color: Colors.blue,
-                                        textColor: Colors.white,
-                                      )
-                                    ],
-                                  ),
-                                  position: info.position,
-                                  opacityFactor: 1,
-                                  translationFactor: 900.0,
-                                )
-                              : Container(),
-                        ],
+                          position: info.position,
+                          opacityFactor: 1,
+                          translationFactor: 900.0,
+                        ),
+                        currentIndex.value == 2
+                            ? //TODO: this a single Widget
+                            ParallaxContainer(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    MaterialButton(
+                                      height: 50,
+                                      minWidth: 150,
+                                      onPressed: () {},
+                                      child: Text('OKAY'),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: new BorderRadius.circular(25.0),
+                                      ),
+                                      color: Colors.blue,
+                                      textColor: Colors.white,
+                                    )
+                                  ],
+                                ),
+                                position: info.position,
+                                opacityFactor: 1,
+                                translationFactor: 900.0,
+                              )
+                            : Container(),
+                      ],
+                    ),
+                  );
+                }),
+              ),
+              currentIndex.value != 2
+                  ? Positioned(
+                      bottom: 8,
+                      left: 8,
+                      right: 8,
+                      child: Container(
+                        height: 50,
+                        width: double.infinity,
+                        child: Center(
+                          child: DotsIndicator(
+                            dotsCount: 3,
+                            position: currentIndex.value.toDouble(),
+                            decorator: DotsDecorator(
+                              size: const Size.square(9.0),
+                              activeSize: const Size(18.0, 9.0),
+                              activeShape:
+                                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                            ),
+                          ),
+                        ),
                       ),
-                    );
-                  }),
-                ),
-                DotsIndicator(
-                  dotsCount: 3,
-                  position: currentIndex.value.toDouble(),
-                  decorator: DotsDecorator(
-                    size: const Size.square(9.0),
-                    activeSize: const Size(18.0, 9.0),
-                    activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-                  ),
-                )
-              ],
-            );
-          },
-        ));
+                    )
+                  : Container(),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
