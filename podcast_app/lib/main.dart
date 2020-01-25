5d5b3e6e2718dbe688f2b91f981316bd29c44f1c
Marvin Knabe <marvin.boeddeker@gmail.com>
2020-01-25 02:08:49 +0100

    Debug Banner

--- podcast_app/lib/main.dart
+++ podcast_app/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Podcast App',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
             primarySwatch: Colors.blue,
             textTheme: GoogleFonts.robotoTextTheme(
